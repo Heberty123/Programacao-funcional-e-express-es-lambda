6f64eadfa7676f383a10d47ec3e4cc8b4c4996b7
heberty <hebertyprado22005@gmail.com>
2022-06-17 18:28:13 +0100

    second commit

---
+++ Consumer/Inline-lambda-expression/src/Program.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Program {
+
+    public static void main(String argc[]){
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        double factor = 1.1;
+
+        list.forEach(p -> p.setPrice(p.getPrice() * factor));
+
+        list.forEach(System.out::println);
+    }
+}
---
+++ Predicate/Method-reference-static/src/Program.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String argc[]){
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(Product::staticProductPredicate);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
